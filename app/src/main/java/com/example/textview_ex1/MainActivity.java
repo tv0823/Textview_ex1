77f340d2a8e679d60ae75e7771f16bf6546b806c
tv0823 <tv0823@bs.amalnet.k12.il>
2023-12-19 09:02:35 +0200

    final

--- app/src/main/java/com/example/textview_ex1/MainActivity.java
+++ app/src/main/java/com/example/textview_ex1/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.textview_ex1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btn;
+    TextView tV;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn = findViewById(R.id.btn);
+        tV = findViewById(R.id.tV);
+    }
+
+    public void pressed(View view) {
+        tV.setTextColor(Color.RED);
+        tV.setText("Oh, yea, I’ve been clicked!");
     }
 }
